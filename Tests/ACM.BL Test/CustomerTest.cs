0ca03563ae4b26e56de969a6c2e0165115385118
Gele <darkogele@hotmail.com>
2019-03-24 22:56:57 +0100

    Initial setup

---
+++ Tests/ACM.BL Test/CustomerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL_Test
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValidation()
+        {
+            //--Arrage
+            var customer = new Customer
+            {
+                FirstName = "Darko",
+                LastName = "Gelevski"
+            };
+            var expected = "Gelevski, Darko";
+
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Tests/ACM.BL Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BL Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BL Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("737fb6bf-6553-45e6-bd9e-901e731c5ff5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
